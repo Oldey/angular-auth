9b176932be5775dbfe168c677340b37a04852e2e
Oldey <bm.sange@gmail.com>
2016-12-28 08:32:38 +0300

    ! fixed the issue with dots being moved off the map

--- app/map/map.component.js
+++ app/map/map.component.js
@@ -14,6 +14,10 @@ app
             this.userInfo = JSON.parse(JSON.stringify(this.auth));
             //this.userInfo = this.auth; // this.userInfo.dots - array of dots with properties: amount, name, x, y
             this.dots = this.userInfo.dots;
+            this.canvasWidth;
+            this.canvasHeight;
+            this.dotRadius = 10;
+            this.dotOffset = this.dotRadius / 10;
             this.current = {};
             this.currentIndex;
             this.getPercentW = {};
@@ -30,28 +34,28 @@ app
             
             angular.element(document).ready(function () {
 
-                self.s = Snap('#SVG');
+                self.canvas = Snap('#SVG');
                 //document.getElementById('SVG').setAttribute('width', 1000);document.getElementById('SVG').setAttribute('height', 1000);
                 document.getElementById('SVG').setAttribute('width', document.getElementById('SVG').getBoundingClientRect().width);
                 document.getElementById('SVG').setAttribute('height', document.getElementById('SVG').getBoundingClientRect().height); 
         
-                var w = +self.s.attr('width');
-                var h = +self.s.attr('height');  
+                self.canvasWidth = +self.canvas.attr('width');
+                self.canvasHeight = +self.canvas.attr('height');  
                 
                 self.getPercentW = function(coord) {
-                    return coord / w * 100;
+                    return coord / self.canvasWidth * 100;
                 }
                 
                 self.getCoordW = function(percent) {
-                    return percent * w / 100;
+                    return percent * self.canvasWidth / 100;
                 }
                 
                 self.getPercentH = function(coord) {
-                    return coord / h * 100;
+                    return coord / self.canvasHeight * 100;
                 }
                 
                 self.getCoordH = function(percent) {
-                    return percent * h / 100;
+                    return percent * self.canvasHeight / 100;
                 }
                 
                 self.updateCurrent = function(item) {
@@ -62,46 +66,47 @@ app
                         y: item.y
                     };
                 }
-                
+
+                // The idea to limit the drag range of each circle is from here http://bl.ocks.org/mbostock/1557377
+                // x is being limited to the range [radius, canvasWidth - radius]
+                // y is being limited to the range [radius, canvasHeight - radius]
                 self.registerDragEventsHandler = function(item, i) {
                     item.dot.drag(
-                        function (dx, dy, posx, posy) {
-                            this.attr( { cx: posx + window.pageXOffset - document.querySelector('.wrapper').offsetLeft, cy: posy + window.pageYOffset - document.querySelector('.header').clientHeight } );
+                        function (dx, dy, posx, posy) { // drag move
+                            var cx = posx + window.pageXOffset - document.querySelector('.wrapper').offsetLeft,
+                                cy = posy + window.pageYOffset - document.querySelector('.header').clientHeight;
+                            this.attr({ 
+                                cx: Math.max(self.dotOffset, Math.min(self.canvasWidth - self.dotOffset, cx)), 
+                                cy: Math.max(self.dotOffset, Math.min(self.canvasHeight - self.dotOffset, cy))
+                            });
                             item.x = self.getPercentW(this.attr('cx'));
                             item.y = self.getPercentH(this.attr('cy'));
-                            AuthService.saveMapChanges(self.userInfo);
                             $scope.$apply(function() {
                                 self.current = self.updateCurrent(item);
                             });
-                        }, 
-                        function() {
+                        },
+                        function() { // drag start
                             //console.log("Move started" + " cx = " + this.attr('cx') + " cy = " + this.attr('cy'));
                             $scope.$apply(function() {
                                 self.current = self.updateCurrent(item);
                             });
                             self.currentIndex = i;
                         },
-                        function() {
-                            //console.log("Move stopped" + " cx = " + this.attr('cx') + " cy = " + this.attr('cy'));
-                            item.x = self.getPercentW(this.attr('cx'));
-                            item.y = self.getPercentH(this.attr('cy'));
+                        function() { // drag end
                             AuthService.saveMapChanges(self.userInfo);
-                            $scope.$apply(function() {
-                                self.current = self.updateCurrent(item);
-                            });
                         }
                     );
                 }
                 
                 self.dots.forEach(function(item, i, dots) {
                     if (item) {
-                        item.dot = self.s.circle(self.getCoordW(item.x), self.getCoordH(item.y), 10);
+                        item.dot = self.canvas.circle(self.getCoordW(item.x), self.getCoordH(item.y), self.dotRadius);
                         self.registerDragEventsHandler(item, i);
                     }});
             });
 
             this.add = () => { // TODO manage adding an existent dot
-                var dot = self.s.circle(self.getCoordW(this.current.x), self.getCoordH(this.current.y), 10);
+                var dot = self.canvas.circle(self.getCoordW(this.current.x), self.getCoordH(this.current.y), self.dotRadius);
                 self.dots.push({
                     name: this.current.name,
                     amount: this.current.amount,
